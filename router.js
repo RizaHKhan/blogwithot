d425d4036b3dc131cd5129b8a4570b3061974fe5
Riza Khan <khanriza@gmail.com>
2019-09-26 17:16:26 -0400

    Procfile adjustment

--- router.js
+++ router.js
@@ -2,8 +2,6 @@ const express = require('express')
 const router = express.Router()
 const userController = require('./controller/userController')
 
-
 router.get('/', userController.home)
-
 
 module.exports = router
